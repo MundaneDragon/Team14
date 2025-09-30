d1917b83dffde2e054ae25f5f2664bd14ff396b3
Run Shan Zhou <runshanzhou1@gmail.com>
2025-10-01 00:05:12 +1000

    Merged in brian's changes

--- my-app/src/app/network/page.js
+++ my-app/src/app/network/page.js
@@ -1,14 +1,56 @@
 import MainBody from "../components/mainBody"
+import CloseIcon from '@mui/icons-material/Close';
+
 export default function Network() {
     return (
         <MainBody>
             <div className="flex flex-col gap-4">
-                <h1 className="text-2xl font-semibold py-2">
+                <h1 className="text-2xl font-semibold py-2 ml-7">
                     Planned Networking Events
                 </h1>
-                
+                <div className="flex flex-col gap-2">
+                    <NetworkCard/>
+                    <NetworkCard/>
+                    <NetworkCard/>
+                </div>
             </div>
         </MainBody>
     )
 }
 
+
+
+function NetworkCard({data}) {
+
+
+    return (
+    <div className="flex gap-4 p-4 hover:bg-gray-400/20 cursor-pointer rounded-xl hover:scale-105 transition-all duration-300">
+        <div className="flex gap-2 items-center">
+            <div className="h-10/12 w-1 bg-red-300 rounded-md">
+            </div>
+            <img src="/society.webp" className="w-80 h-42 rounded-md"/>
+        </div>
+        <div className="flex flex-col justify-between">
+            <div className="">
+                <h2 className="font-semibold text-xl">
+                    2025 CEUS AGM
+                </h2>
+                <div className="mt-4 flex flex-col text-gray-400">
+                    <p>
+                        Happening in 10 min
+                    </p>
+                    <p>
+                        20 people planning to network
+                    </p>
+                </div>
+            </div>
+            <button className="bg-[#FFA3A3] w-64 rounded-md text-black py-2 cursor-pointer hover:bg-[#f58888] flex justify-center items-center gap-2">
+                Remove 
+                <div className="pt-0.5">
+                    <CloseIcon/>
+                </div>
+            </button>
+        </div>
+    </div>
+    )
+}
