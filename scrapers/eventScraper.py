c6dc1810432bf54a586ed9dd0b62e50ff2dec5a0
Daivaaddia <dkfaddia2gmail.com>
2025-10-02 09:43:58 +1000

    fix env

--- scrapers/eventScraper.py
+++ scrapers/eventScraper.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-url = os.environ.get("NEXT_PUBLIC_SUPABASE_URL")
+url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_SERVICE_ROLE_KEY") 
 supabase = create_client(url, key)
 
